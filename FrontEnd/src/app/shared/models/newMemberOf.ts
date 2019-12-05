ba0bd1f8eff4a0a9fe5df6b6d15568ed72de3aa1
Jessica Kwok <jlk187@case.edu>
2019-12-05 13:56:50 -0500

    deleted newMemberOf

--- FrontEnd/src/app/shared/models/newMemberOf.ts
+++
@@ -1,9 +0,0 @@
-export class NewMemberOf {
-    sid: number;
-    chapterName: string;
-
-    constructor(sid, chapterName) {
-        this.sid = sid;
-        this.chapterName = chapterName;
-    }
-}
